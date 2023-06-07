6058bf5eebf59484162b6499dbc8ab1b5c34f61c
Nick <Nicholasmaassen@yahoo.com>
2023-06-07 17:00:10 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "nmaass.github.io/Portfolio/precache-manifest.3d3e09ce9266e19f36071c74cc9c5293.js"
+  "/Portfolio/precache-manifest.798b1f0117c351ea8ca71b746965e952.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("nmaass.github.io/Portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/Portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
