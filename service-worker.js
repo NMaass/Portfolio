09c43a86bd8a952460437485c11a9bfa8c20cf8a
Nick <Nicholasmaassen@yahoo.com>
2023-06-15 07:58:18 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.85bd39ea39d225076bd3b100840ea750.js"
+  "/Portfolio/precache-manifest.9d113d65f97ae565c27e692674fd07e6.js"
 );
 
 workbox.clientsClaim();
